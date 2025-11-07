fcc8c322f0cba5e7f02217a898ff0960dd75bf37
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-11-06 19:22:16 -0700

    Add project files.

---
+++ Datalogger/DataLogger.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DataLogger
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonGroupBox = New System.Windows.Forms.GroupBox()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.GraphButton = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GraphPictureBox = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ButtonGroupBox.SuspendLayout()
+        CType(Me.GraphPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ButtonGroupBox
+        '
+        Me.ButtonGroupBox.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ButtonGroupBox.Controls.Add(Me.ExitButton)
+        Me.ButtonGroupBox.Controls.Add(Me.GraphButton)
+        Me.ButtonGroupBox.Controls.Add(Me.Button1)
+        Me.ButtonGroupBox.Location = New System.Drawing.Point(506, 351)
+        Me.ButtonGroupBox.Name = "ButtonGroupBox"
+        Me.ButtonGroupBox.Size = New System.Drawing.Size(282, 87)
+        Me.ButtonGroupBox.TabIndex = 6
+        Me.ButtonGroupBox.TabStop = False
+        Me.ButtonGroupBox.Text = "GroupBox1"
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(191, 21)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(86, 56)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&xitButton"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'GraphButton
+        '
+        Me.GraphButton.Location = New System.Drawing.Point(99, 21)
+        Me.GraphButton.Name = "GraphButton"
+        Me.GraphButton.Size = New System.Drawing.Size(86, 56)
+        Me.GraphButton.TabIndex = 2
+        Me.GraphButton.Text = "&Graph"
+        Me.GraphButton.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(7, 21)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(86, 56)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GraphPictureBox
+        '
+        Me.GraphPictureBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GraphPictureBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.GraphPictureBox.Location = New System.Drawing.Point(12, 27)
+        Me.GraphPictureBox.Name = "GraphPictureBox"
+        Me.GraphPictureBox.Size = New System.Drawing.Size(776, 318)
+        Me.GraphPictureBox.TabIndex = 5
+        Me.GraphPictureBox.TabStop = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 28)
+        Me.MenuStrip1.TabIndex = 7
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ButtonGroupBox)
+        Me.Controls.Add(Me.GraphPictureBox)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ButtonGroupBox.ResumeLayout(False)
+        CType(Me.GraphPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonGroupBox As GroupBox
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents GraphButton As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GraphPictureBox As PictureBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+End Class
